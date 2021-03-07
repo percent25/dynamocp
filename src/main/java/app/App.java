4cfd049a47b321a9aafc8347aa5782d217ee2f91
rrizun <rrizun>
2021-03-07 09:26:36 -0800

    checkpoint

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -8,10 +8,14 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.UUID;
+import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.zip.GZIPInputStream;
@@ -19,15 +23,20 @@ import java.util.zip.GZIPOutputStream;
 
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.CaseFormat;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Range;
 import com.google.common.io.BaseEncoding;
 import com.google.common.io.CharStreams;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.RateLimiter;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -61,10 +70,12 @@ import software.amazon.awssdk.utils.ImmutableMap;
 
 class AppState {
   public final AtomicLong count = new AtomicLong();
-  public final List<Map<String, AttributeValue>> exclusiveStartKeys = Lists.newArrayList(); // exclusiveStartKeys len *is* totalSegments
+  public final List<Map<String, AttributeValue>> exclusiveStartKeys = Lists.newArrayList();
+
   public int totalSegments() {
     return exclusiveStartKeys.size();
   }
+
   public String toString() {
     return new Gson().toJson(this);
   }
@@ -81,6 +92,7 @@ class AppOptions {
   public String resume; // base64 encoded gzipped app state
   //
   public String transform_expression;
+
   //
   public String toString() {
     return new Gson().toJson(this);
@@ -102,13 +114,8 @@ public class App implements ApplicationRunner {
   private String targetTable;
 
   private AppState appState = new AppState();
-
-  // aws sdk 2
-  private final DynamoDbClient dynamo = DynamoDbClient.create();
-  private final DynamoDbAsyncClient dynamoAsync = DynamoDbAsyncClient.create();
-
-  // @see totalSegments
-  private final List<Thread> threads = Lists.newArrayList();
+  
+  private final DynamoDbAsyncClient dynamoAsync = DynamoDbAsyncClient.builder().build();
 
   /**
    * ctor
@@ -176,112 +183,97 @@ public class App implements ApplicationRunner {
 
     // log thread count
     log(Range.closedOpen(0, appState.totalSegments()));
-    
-    for (int segment = 0; segment < appState.exclusiveStartKeys.size(); ++segment) {
-      final int finalSegment = segment;
-      threads.add(new Thread() {
 
-        // https://aws.amazon.com/blogs/developer/rate-limited-scans-in-amazon-dynamodb/
-        int permits = 128; // worst-case unbounded scan read capacity units consumed (initial estimate)
+    new FutureRunner() {
+      {
+        for (int segment = 0; segment < appState.exclusiveStartKeys.size(); ++segment)
+          doSegment(segment, 128);
+      }
 
-        @Override
-        public void run() {
-          rcuMeter().mark(0);
+      void doSegment(int segment, int permits) {
+        run(() -> {
+          if (permits > 0)
+            readLimiter.acquire(permits);
+
+          // STEP 1 Do the scan
+          ScanRequest scanRequest = ScanRequest.builder()
+              //
+              .tableName(sourceTable)
+              //
+              .exclusiveStartKey(appState.exclusiveStartKeys.get(segment))
+              //
+              .returnConsumedCapacity(ReturnConsumedCapacity.TOTAL)
+              //
+              .segment(segment)
+              //
+              .totalSegments(appState.exclusiveStartKeys.size())
+              // //
+              // .limit(options.scanLimit > 0 ? options.scanLimit : null)
+              //
+              .build();
+          return lf(dynamoAsync.scan(scanRequest));
+        }, scanResponse -> {
           try {
-            // log("run", finalSegment);
-            do {
 
-              if (permits > 0)
-                readLimiter.acquire(permits);
+            appState.exclusiveStartKeys.set(segment, scanResponse.lastEvaluatedKey());
 
-              // STEP 1 Do the scan
-              ScanRequest scan = ScanRequest.builder()
-                  //
-                  .tableName(sourceTable)
-                  //
-                  .exclusiveStartKey(appState.exclusiveStartKeys.get(finalSegment))
-                  //
-                  .returnConsumedCapacity(ReturnConsumedCapacity.TOTAL)
-                  //
-                  .segment(finalSegment)
-                  //
-                  .totalSegments(appState.exclusiveStartKeys.size())
-                  // //
-                  // .limit(options.scanLimit > 0 ? options.scanLimit : null)
-                  //
-                  .build();
+            // STEP 2 Process results here
 
-              ScanResponse result = dynamo.scan(scan);
+            if (targetTable == null) {
 
-              appState.exclusiveStartKeys.set(finalSegment, result.lastEvaluatedKey());
+              readCount.addAndGet(scanResponse.items().size());
 
-              Double consumedCapacityUnits = result.consumedCapacity().capacityUnits();
-
-              rcuMeter().mark(consumedCapacityUnits.longValue());
-
-              permits = consumedCapacityUnits.intValue();
-
-              // STEP 2 Process results here
-
-              if (targetTable == null) {
-
-                readCount.addAndGet(result.items().size());
-
-                log(readCount.get(),
-                    //
-                    String.format("%s/%s",
-                        Double.valueOf(rcuMeter().getMeanRate()).intValue(),
-                        Double.valueOf(wcuMeter().getMeanRate()).intValue()),
-                    //
-                    renderState(appState));
-
-                  // for (Map<String, AttributeValue> item : result.items())
-                  //   out(item);
-
-              } else {
-
-                List<Map<String, AttributeValue>> items = new ArrayList<>();
-                for (Map<String, AttributeValue> item : result.items()) {
-                  item = new HashMap<>(item); // deep copy
-                  items.add(item);
-                  //###TODO
-                  //###TODO
-                  //###TODO
-                    // transform
-                    // item.put("key", AttributeValue.builder().s(UUID.randomUUID().toString()).build());
-                  //###TODO
-                  //###TODO
-                  //###TODO
+              for (Map<String, AttributeValue> item : scanResponse.items()) {
+                System.out.println(render(item));
               }
 
-                doWrite(items);
+                    // log(readCount.get(),
+                    //     //
+                    //     String.format("%s/%s", Double.valueOf(rcuMeter().getMeanRate()).intValue(),
+                    //         Double.valueOf(wcuMeter().getMeanRate()).intValue()),
+                    //     //
+                    //     renderState(appState));
 
+                  System.err.println(renderState(appState));
+
+
+              // for (Map<String, AttributeValue> item : scanResponse.items())
+              //   out(item);
+
+            } else {
+
+              List<Map<String, AttributeValue>> items = new ArrayList<>();
+              for (Map<String, AttributeValue> item : scanResponse.items()) {
+                item = new HashMap<>(item); // deep copy
+                items.add(item);
+                System.out.println(item);
               }
+              doWrite(items);
+            }
 
-              // log(appState.count.get(),
-              // //
-              // new Double(rcuMeter().getMeanRate()).intValue(),
-              // //
-              // new Double(wcuMeter().getMeanRate()).intValue(),
-              // //
-              // renderState(appState));
-
-            } while (!appState.exclusiveStartKeys.get(finalSegment).isEmpty());
+            if (!appState.exclusiveStartKeys.get(segment).isEmpty())
+              doSegment(segment, scanResponse.consumedCapacity().capacityUnits().intValue());
 
           } catch (Exception e) {
             throw new RuntimeException(e);
           }
+        });
+      }
+    }.get().get();
+  }
 
-        }
-      });
+  private Map<String, AttributeValue> parse() {
+    return null;
+  }
 
-    }
-
-    for (Thread thread : threads)
-      thread.start();
-    for (Thread thread : threads)
-      thread.join();
-
+  private String render(Map<String, AttributeValue> item) throws Exception {
+    return new Gson().toJson(Maps.transformValues(item, value -> {
+      try {
+        return new Gson().fromJson(new ObjectMapper().writeValueAsString(value.toBuilder()), JsonElement.class);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }));
   }
 
   private void doWrite(List<Map<String, AttributeValue>> items) throws Exception {
---
+++ src/main/java/app/FutureRunner.java
@@ -0,0 +1,191 @@
+package app;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
+
+import com.google.common.util.concurrent.AbstractFuture;
+import com.google.common.util.concurrent.AsyncCallable;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.spotify.futures.CompletableFuturesExtra;
+
+/**
+ * Opinionated robust facade/runner for listenablefuture(s).
+ */
+public class FutureRunner {
+    
+    private class VoidFuture extends AbstractFuture<Void> {
+        public boolean setVoid() {
+            return super.set(null);
+        }
+        public boolean setException(Throwable throwable) {
+            return super.setException(throwable);
+        }
+    }
+      
+    private int running; // aka inFlight
+    private final Object lock = new Object();
+    private final VoidFuture facade = new VoidFuture();
+    private final List<ListenableFuture<?>> insideFutures = new CopyOnWriteArrayList<>();
+    private Exception firstException;
+
+    /**
+     * ctor
+     */
+    public FutureRunner() {
+        facade.addListener(()->{
+            if (facade.isCancelled())
+                insideFutures.forEach(insideFuture -> insideFuture.cancel(true));
+        }, MoreExecutors.directExecutor());
+    }
+
+    public ListenableFuture<?> get() {
+        synchronized (lock) {
+            doFinally(); // safety
+            return facade;
+        }
+    }
+
+    /**
+     * run
+     * 
+     * @param <T>
+     * @param request
+     */
+    protected <T> void run(AsyncCallable<T> request) {
+        run(request, unused -> {});
+    }
+
+    /**
+     * run
+     * 
+     * @param <T>
+     * @param request
+     * @param response
+     */
+    protected <T> void run(AsyncCallable<T> request, Consumer<T> response) {
+        run(request, response, e -> { throw new RuntimeException(e); });
+    }
+
+    /**
+     * run
+     * 
+     * @param <T>
+     * @param request
+     * @param perRequestResponseFinally
+     */
+    protected <T> void run(AsyncCallable<T> request, Runnable perRequestResponseFinally) {
+        run(request, unused -> {}, e->{ throw new RuntimeException(e); }, perRequestResponseFinally);
+    }
+
+    /**
+     * run
+     * 
+     * @param <T>
+     * @param request
+     * @param response
+     * @param perRequestResponseCatch
+     */
+    protected <T> void run(AsyncCallable<T> request, Consumer<T> response, Consumer<Exception> perRequestResponseCatch) {
+        run(request, response, perRequestResponseCatch, () -> {});
+    }
+
+    /**
+     * run
+     * 
+     * @param <T>
+     * @param request
+     * @param response
+     * @param perRequestResponseCatch
+     * @param perRequestResponseFinally
+     */
+    protected <T> void run(AsyncCallable<T> request, Consumer<T> response, Consumer<Exception> perRequestResponseCatch, Runnable perRequestResponseFinally) {
+        synchronized (lock) {
+            try {
+                ListenableFuture<T> lf = request.call(); // throws
+                ++running;
+                insideFutures.add(lf);
+                lf.addListener(() -> {
+                    synchronized (lock) {
+                        --running;
+                        try {
+                            response.accept(lf.get()); // throws
+                        } catch (Exception e) {
+                            try {
+                                perRequestResponseCatch.accept(e); // throws
+                            } catch (Exception e1) {
+                                doException(e1);
+                            }
+                        } finally {
+                            try {
+                                perRequestResponseFinally.run(); // throws
+                            } catch (Exception e2) {
+                                doException(e2);
+                            } finally {
+                                doFinally();
+                            }
+                        }
+                    }
+                }, MoreExecutors.directExecutor());
+            } catch (Exception e) {
+                insideFutures.add(Futures.immediateFailedFuture(e));
+                try {
+                    try {
+                        perRequestResponseCatch.accept(e); // throws
+                    } catch (Exception e1) {
+                        doException(e1);
+                    }
+                } finally {
+                    try {
+                        perRequestResponseFinally.run(); // throws
+                    } catch (Exception e2) {
+                        doException(e2);
+                    } finally {
+                        doFinally();
+                    }
+                }
+            }
+        }
+    }
+
+    // convenience
+    protected void onFinally() {
+        // do nothing
+    }
+
+    // convenience
+    protected <T> ListenableFuture<T> lf(CompletableFuture<T> cf) {
+        return CompletableFuturesExtra.toListenableFuture(cf);
+    }
+
+    // ----------------------------------------------------------------------
+
+    private void doException(Exception e) {
+        if (firstException == null)
+            firstException = e;
+    }
+
+    private void doFinally() {
+        if (running == 0) {
+            --running; // once
+            try {
+                onFinally();
+            } catch (Exception e) {
+                doException(e);
+            } finally {
+                if (firstException == null)
+                    facade.setVoid();
+                else
+                    facade.setException(firstException);
+            }
+        }
+    }
+
+    private void log(Object... args) {
+        new LogHelper(this).log(args);
+    }
+
+}
