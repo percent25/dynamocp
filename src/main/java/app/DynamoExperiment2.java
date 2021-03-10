27b0fbbe69b239cc276fd87f369c7964eafe5dd6
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-10 09:58:47 -0800

    DynamoExperiment2

---
+++ src/main/java/app/DynamoExperiment2.java
@@ -0,0 +1,208 @@
+package app;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.google.common.base.Joiner;
+import com.google.common.hash.Hashing;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.RateLimiter;
+import com.google.gson.Gson;
+import com.spotify.futures.CompletableFuturesExtra;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.ReturnConsumedCapacity;
+
+class MyRecord {
+  public boolean success;
+  public String failureMessage;
+  public double rateIn;
+  public double instantaneousRate;
+  public double fastRate;
+  public double slowRate;
+  public double rateOut;
+  public String toString() {
+    return new Gson().toJson(this);
+  }
+}
+
+public class DynamoExperiment2 {
+
+  static AtomicLong id = new AtomicLong();
+
+  static {
+    ((ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger("ROOT")).setLevel(ch.qos.logback.classic.Level.INFO);
+  }
+
+  static DynamoDbAsyncClient client = DynamoDbAsyncClient.builder().httpClient(AwsSdkTwo.httpClient).build();
+  static String tableName = "MyTable";
+
+  static int concurrency = 16;
+  static Semaphore sem = new Semaphore(concurrency);
+
+  static long fastWindow = 60;
+  static long slowWindow = 300;
+
+  static RateLimiter rateLimiter = RateLimiter.create(5.0);
+  static LocalMeter reportedMeter = new LocalMeter();
+  // static LocalMeter secondMeter = new LocalMeter();
+
+  static final Object lock = new Object();
+
+
+  public static void main(String... args) throws Exception {
+
+    rateLimiter.acquire(Double.valueOf(rateLimiter.getRate()).intValue());
+
+    // prime client
+    log(client.describeTable(DescribeTableRequest.builder().tableName(tableName).build()).get());
+
+    // // prime local meter
+    // localMeter.mark(Double.valueOf(rateLimiter.getRate()).longValue()*fastWindow);
+
+    while (true) {
+
+      var myRecord = new MyRecord();
+
+      sem.acquire();
+
+      var t0 = System.currentTimeMillis();
+
+      try {
+        String key = Hashing.sha256().hashLong(id.incrementAndGet()).toString();
+        
+        var item = new HashMap<String, AttributeValue>();
+        item.put("id", AttributeValue.builder().s(key).build());
+        item.put("val1", AttributeValue.builder().s(UUID.randomUUID().toString()).build());
+        item.put("val2", AttributeValue.builder().s(UUID.randomUUID().toString()).build());
+        item.put("val3", AttributeValue.builder().s(UUID.randomUUID().toString()).build());
+
+        var putItemRequest = PutItemRequest.builder().tableName(tableName).item(item).returnConsumedCapacity(ReturnConsumedCapacity.TOTAL).build();
+
+        var lf = lf(client.putItem(putItemRequest));
+
+        lf.addListener(()->{
+          synchronized (lock) {
+          try {
+            var t = System.currentTimeMillis();
+
+            var putItemResponse = lf.get();
+            var consumedCapacityUnits = putItemResponse.consumedCapacity().capacityUnits();
+
+            // real -> adjusted
+            // nominal -> unadjusted
+
+            // instantaneousRate ?!?
+
+            var instantaneousRate = Double.valueOf(1000.0 * consumedCapacityUnits / (t - t0) * concurrency).doubleValue();
+            myRecord.instantaneousRate = instantaneousRate;
+
+            rateLimiter.acquire(consumedCapacityUnits.intValue());
+            reportedMeter.mark(consumedCapacityUnits.longValue());
+
+            // 80.0 -> 0.5    0.0/0.0
+
+            var fastRate = reportedMeter.avg(fastWindow).doubleValue();
+            myRecord.fastRate = reportedMeter.avg(fastWindow).doubleValue();
+
+            var slowRate = reportedMeter.avg(slowWindow).doubleValue();
+            myRecord.slowRate = reportedMeter.avg(slowWindow).doubleValue();
+
+            var rateIn = rateLimiter.getRate();
+
+            myRecord.rateIn = rateIn;
+            // log("rate", rate, "delta", delta);
+
+            double factor = 1;
+
+            // speeding up?
+            if (fastRate > slowRate) {
+
+              // we desire to achieve instantaneousRate or fastRate
+              // the gap to instantaneousRate is... from where we are now to instantaneousRate
+
+              // "rateIn":5.0,"instantaneousRate":56.737588652482266,"fastRate":0.2,"slowRate":0.1,"rateOut":30.868794326241133}
+              // "rateIn":30.868794326241133,"instantaneousRate":36.03603603603604,"fastRate":0.4,"slowRate":0.2,"rateOut":33.45241518113858}
+
+              var absDesire = Math.max(instantaneousRate, fastRate);
+              var whereWeAreNow = rateIn; // Math.max(rateIn, fastRate); //###TODO SHOULD THIS BE JUST RATEIN ?? 
+              var deltaDesire = absDesire - rateIn;
+              var rateOut = absDesire - deltaDesire*slowRate/fastRate / factor;
+
+              myRecord.rateOut = rateOut;
+
+              rateLimiter.setRate(rateOut);
+
+            }
+
+            //.IllegalArgumentException: rate must be positive","rateIn":10.2,"instantaneousRate":0.6476683937823834,"fastRate":0.0,"slowRate":4.6,"rateOut":0.0}
+
+            // slowing down?
+            if (fastRate < slowRate) {
+
+              // we desire to achieve instantaneousRate or fastRate
+              // the gap to fastRate is... from where we are now to fastRate
+
+              // "rateIn":85.41694762895051,"instantaneousRate":89.88764044943821,"fastRate":17.2,"slowRate":42.4,"rateOut":88.07405751282528}
+              // "rateIn":45.958635258974546,"instantaneousRate":47.61904761904762,"fastRate":47.2,"slowRate":59.4,"rateOut":46.29966271340033}
+
+                    // var absDesire = Math.min(instantaneousRate, fastRate);
+              // want lowest non-zero
+              var absDesire = instantaneousRate;
+              if (slowRate > 0) {
+                if (slowRate < instantaneousRate)
+                  absDesire = slowRate;
+              }
+              var whereWeAreNow = rateIn; // Math.min(rateIn, fastRate);
+              var deltaDesire = rateIn - absDesire;
+              var rateOut = absDesire + deltaDesire*fastRate/slowRate / factor;
+
+              myRecord.rateOut = rateOut;
+
+              rateLimiter.setRate(rateOut);
+
+            }
+
+
+            myRecord.success=true;
+
+          } catch (Exception e) {
+            myRecord.failureMessage = ""+e;
+          } finally {
+            sem.release();
+            log(
+              "consumed",
+              // "firstMeter", firstMeter,
+              // "secondMeter", secondMeter,
+              myRecord);
+
+              log(reportedMeter);
+          }
+        } // synchronized
+        }, MoreExecutors.directExecutor());
+
+      } catch (Exception e) {
+        log(e);
+      } finally {
+      }
+    }
+  }
+
+  // convenience
+  static <T> ListenableFuture<T> lf(CompletableFuture<T> cf) {
+    return CompletableFuturesExtra.toListenableFuture(cf);
+  }
+
+  static void log(Object... args) {    
+    System.out.println(Instant.now().toString()+" "+Joiner.on(" ").useForNull("null").join(args));
+  }
+
+}
--- src/main/java/app/LocalMeter.java
+++ src/main/java/app/LocalMeter.java
@@ -3,47 +3,51 @@ package app;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.google.common.util.concurrent.AtomicDouble;
+
 public class LocalMeter {
 
   private final int windowSeconds = 900;
-  private final AtomicLong total = new AtomicLong();
-  private final ConcurrentSkipListMap<Long, Long> values = new ConcurrentSkipListMap<>();
+  private final AtomicDouble total = new AtomicDouble();
+  private final ConcurrentSkipListMap<Long, Double> values = new ConcurrentSkipListMap<>();
 
   public long now() {
     return System.currentTimeMillis();
   }
 
-  public void mark(long value) {
-    total.addAndGet(value);
+  public void mark(Number value) {
     final long now = now();
+    total.addAndGet(value.doubleValue());
     values.headMap(now - windowSeconds * 1000).clear();
     values.compute(now, (k, v) -> {
-      return (v == null ? 0L : v) + value;
+      return (v == null ? 0.0 : v) + value.doubleValue();
     });
   }
 
-  public long avg(long windowSeconds) {
-    return sum(windowSeconds) / windowSeconds;
+  public Number avg(long windowSeconds) {
+    return sum(windowSeconds).doubleValue() / windowSeconds;
   }
 
-  public long sum(long windowSeconds) {
+  public Number sum(long windowSeconds) {
     final long now = now();
-    // values.headMap(now - 15 * 1000).clear();
+    // values.headMap(now - windowSeconds * 1000).clear();
     long fromKey = now - windowSeconds * 1000;
     long toKey = now;
     long sum = 0;
-    for (long value : values.subMap(fromKey, true, toKey, false).values())
+    for (double value : values.subMap(fromKey, true, toKey, false).values())
       sum += value;
     return sum;
   }
 
-  public long total() {
+  public Number total() {
     return total.get();
   }
 
   public String toString() {
-    // return String.format("%s(%s/s)", sum, avg(15));
-    return String.format("%s %s/%s/%s %s/%s/%s", total, avg(1), avg(5), avg(15), avg(60), avg(300), avg(900));
+    // return String.format("%s(%s/s)", total, avg(15).longValue());
+    return String.format("%s %s/%s/%s %s/%s/%s", total.longValue(),
+      avg(1).longValue(), avg(5).longValue(), avg(15).longValue(),
+      avg(60).longValue(), avg(300).longValue(), avg(900).longValue());
   }
 
 }
