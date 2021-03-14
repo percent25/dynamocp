1550aee10e0a6d8cc9d7fd668e443dfa4b5fe3ef
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-14 15:16:48 -0700

    SystemOut: append

--- src/main/java/io/github/awscat/SystemOutOutputPluginProvider.java
+++ src/main/java/io/github/awscat/SystemOutOutputPluginProvider.java
@@ -1,10 +1,12 @@
 package io.github.awscat;
 
+import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.function.Supplier;
 
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
 import org.springframework.boot.ApplicationArguments;
@@ -46,17 +48,46 @@ class SystemOutOutputPlugin implements OutputPlugin {
 // @Service
 public class SystemOutOutputPluginProvider implements OutputPluginProvider {
 
+  class SystemOutOptions {
+    boolean append;
+  }
+
+  class GetWork {
+    boolean success;
+    String failureMessage;
+    final String arg;
+    String target;
+    SystemOutOptions options;
+    public GetWork(String arg) {
+      this.arg = arg;
+    }
+    public String toString() {
+      return getClass().getSimpleName()+new Gson().toJson(this);
+    }
+  }
+
   @Override
-  public Supplier<OutputPlugin> get(String target, ApplicationArguments args) throws Exception {
-    log("get", target);
-    PrintStream out = "-".equals(target) ? System.out : new PrintStream(target);
-    return () -> {
-      try {
-        return new SystemOutOutputPlugin(out);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    };
+  public Supplier<OutputPlugin> get(String arg, ApplicationArguments args) throws Exception {
+    GetWork work = new GetWork(arg);
+    try {
+      work.target = Args.parseArg(work.arg);
+      work.options = Options.parse(work.arg, SystemOutOptions.class);
+      PrintStream out = "-".equals(work.target) ? System.out : new PrintStream(new FileOutputStream(work.target, work.options.append));
+      work.success = true;
+      return () -> {
+        try {
+          return new SystemOutOutputPlugin(out);
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      };
+    } catch (Exception e) {
+      e.printStackTrace();
+      work.failureMessage = ""+e;
+      throw e;
+    } finally {
+      log(work);
+    }
   }
 
   private void log(Object... args) {
