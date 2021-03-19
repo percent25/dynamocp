60b31129628203a6671cc1235678c1addca96e67
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-19 06:07:30 -0700

    LogHelper: log new Date()

--- src/main/java/helpers/LogHelper.java
+++ src/main/java/helpers/LogHelper.java
@@ -1,5 +1,6 @@
 package helpers;
 
+import java.util.Date;
 import java.util.List;
 
 import com.google.common.base.Strings;
@@ -30,7 +31,7 @@ public class LogHelper {
    */
   public void log(Object... args) {
     List<String> parts = Lists.newArrayList();
-    // parts.add(new Date().toString());
+    parts.add(new Date().toString());
     // parts.add(String.format("[%s]", Thread.currentThread().getName()));
     // parts.add(ClassUtils.getShortName(self.getClass()));
     for (Object arg : args)
@@ -39,6 +40,18 @@ public class LogHelper {
   }
 
   public void debug(Object... args) {
+
+    // Object newArgs = new Object[args.length+1];
+    // System.arraycopy(args, 0, newArgs, 1, args.length);
+    // newArgs[0] = extraVar; 
+    // String.format(format, extraVar, args);    
+
+    // static <T> T[] append(T[] arr, T lastElement) {
+    //   final int N = arr.length;
+    //   arr = java.util.Arrays.copyOf(arr, N+1);
+    //   arr[N] = lastElement;
+    //   return arr;
+    // }
     LoggerFactory.getLogger(self.getClass()).debug(Strings.repeat("{} ", args.length), args);
   }
 
