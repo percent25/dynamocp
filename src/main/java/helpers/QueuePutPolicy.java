bc4a07df661f207f1cb305745b2d7b3d35f9f2c7
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-20 05:16:52 -0700

    QueuePutPolicy

---
+++ src/main/java/helpers/QueuePutPolicy.java
@@ -0,0 +1,19 @@
+package helpers;
+
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class QueuePutPolicy implements RejectedExecutionHandler {
+
+	@Override
+	public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+		try {
+			executor.getQueue().put(r);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new RejectedExecutionException(e);
+		}
+	}
+
+}
