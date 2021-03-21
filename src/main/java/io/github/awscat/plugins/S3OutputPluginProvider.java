8e05f65773d269d0c688a7ff8e1de170cc1a00f8
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-21 12:48:06 -0700

    s3provider: toString()

--- src/main/java/io/github/awscat/plugins/S3OutputPluginProvider.java
+++ src/main/java/io/github/awscat/plugins/S3OutputPluginProvider.java
@@ -3,6 +3,8 @@ package io.github.awscat.plugins;
 import java.net.URI;
 import java.util.function.Supplier;
 
+import com.google.common.base.MoreObjects;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,28 @@ import io.github.awscat.OutputPlugin;
 import io.github.awscat.OutputPluginProvider;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 
+// delegates toString to the provider
+class AbstractSupplier implements Supplier<OutputPlugin> {
+
+    private final OutputPluginProvider provider;
+    private final Supplier<OutputPlugin> supplier;
+
+    AbstractSupplier(OutputPluginProvider provider, Supplier<OutputPlugin> supplier) {
+        this.provider = provider;
+        this.supplier = supplier;
+    }
+
+    @Override
+    public OutputPlugin get() {
+        return supplier.get();
+    }
+
+    public String toString() {
+        return provider.toString();
+    }
+
+}
+
 @Service
 public class S3OutputPluginProvider implements OutputPluginProvider {
 
@@ -20,6 +44,11 @@ public class S3OutputPluginProvider implements OutputPluginProvider {
 
     public S3OutputPluginProvider(ApplicationArguments args) {
         this.args = args;
+    }
+
+    public String toString() {
+        String arg = args.getNonOptionArgs().get(1);
+        return MoreObjects.toStringHelper(this).add("arg", arg).toString();
     }
 
     @Override
@@ -37,13 +66,12 @@ public class S3OutputPluginProvider implements OutputPluginProvider {
         String bucket = uri.getHost();
         String exportPrefix = uri.getPath().substring(1);
 
-        // Note- aws s3 transport is thread safe
+        // Note- transport is thread safe
         var transport = new ConcatenatedJsonWriterTransportAwsS3Export(client, bucket, exportPrefix);
-        return () -> {
-            // Note- ConcatenatedJsonWriter is not thread safe
-            // therefore need to return a new instance every invocation
+        // Note- ConcatenatedJsonWriter is not thread safe
+        return new AbstractSupplier(this, ()->{
             return new ConcatenatedJsonWriterOutputPlugin(new ConcatenatedJsonWriter(transport));
-        };
+        });
     }
-    
+
 }
