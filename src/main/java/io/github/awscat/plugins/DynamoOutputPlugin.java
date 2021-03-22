592ef3bb47c0288f6133d52cec8ea1c9c304e8f8
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-21 17:21:29 -0700

    don't include keySchema in static options

--- src/main/java/io/github/awscat/plugins/DynamoOutputPlugin.java
+++ src/main/java/io/github/awscat/plugins/DynamoOutputPlugin.java
@@ -67,15 +67,12 @@ class DynamoOutputPluginProvider implements OutputPluginProvider {
   }
 
   private String tableName;
-  private Iterable<String> keySchema;
   private Options options;
 
   public String toString() {
     return MoreObjects.toStringHelper("DynamoOutputPlugin")
         //
         .add("tableName", tableName)
-        //
-        .add("keySchema", keySchema)
         //
         .add("options", options)
         //
@@ -89,66 +86,37 @@ class DynamoOutputPluginProvider implements OutputPluginProvider {
 
   @Override
   public boolean canActivate(String arg) {
+    tableName = Args.base(arg).split(":")[1];
+    options = Args.options(arg, Options.class);
     return ImmutableSet.of("dynamo", "dynamodb").contains(arg.split(":")[0]);
   }
 
   @Override
   public Supplier<OutputPlugin> activate(String arg) throws Exception {
-    tableName = Args.base(arg).split(":")[1];
-
     DynamoDbAsyncClient client = DynamoDbAsyncClient.builder().build();
     DescribeTableRequest describeTableRequest = DescribeTableRequest.builder().tableName(tableName).build();
     DescribeTableResponse describeTableResponse = client.describeTable(describeTableRequest).get();
 
-    keySchema = Lists.transform(describeTableResponse.table().keySchema(), e->e.attributeName());      
+    Iterable<String> keySchema = Lists.transform(describeTableResponse.table().keySchema(), e->e.attributeName());      
 
     int availableProcessors = Runtime.getRuntime().availableProcessors();
-    int provisionedWcu = describeTableResponse.table().provisionedThroughput().writeCapacityUnits().intValue();
-
-    options = Args.options(arg, Options.class);
     options.c = options.c > 0 ? options.c : availableProcessors;
+
+    int provisionedWcu = describeTableResponse.table().provisionedThroughput().writeCapacityUnits().intValue();
     options.wcu = options.wcu > 0 ? options.wcu : provisionedWcu;
 
     var c = new Semaphore(options.c);
     var writeLimiter = RateLimiter.create(options.wcu > 0 ? options.wcu : Integer.MAX_VALUE);
 
-    Supplier<?> preWarm = Suppliers.memoize(() -> {
-      if (options.wcu > 0)
-        writeLimiter.acquire(options.wcu);
-      return Defaults.defaultValue(Void.class);
-    });
+        // Supplier<?> preWarm = Suppliers.memoize(() -> {
+        //   if (options.wcu > 0)
+        //     writeLimiter.acquire(options.wcu);
+        //   return Defaults.defaultValue(Void.class);
+        // });
 
     return new AbstractSupplier(this, ()->{
       return new DynamoOutputPlugin(client, tableName, keySchema, c, writeLimiter, options.delete);
     });
-    // return new Supplier<OutputPlugin>() {
-    //   @Override
-    //   public OutputPlugin get() {
-    //     //###TODO REALLY NEEDED??
-    //     //###TODO REALLY NEEDED??
-    //     //###TODO REALLY NEEDED??
-    //     preWarm.get();
-    //     //###TODO REALLY NEEDED??
-    //     //###TODO REALLY NEEDED??
-    //     //###TODO REALLY NEEDED??
-    //     return new DynamoOutputPlugin(client, tableName, keySchema, c, writeLimiter, options.delete);
-    //   }
-    //   public String toString() {
-    //     return MoreObjects.toStringHelper("DynamoOutputPlugin")
-    //         //
-    //         .add("tableName", tableName)
-    //         //
-    //         .add("keySchema", keySchema)
-    //         //
-    //         .add("c", options.c)
-    //         //
-    //         .add("wcu", options.wcu)
-    //         //
-    //         .add("delete", options.delete)
-    //         //
-    //         .toString();
-    //   }
-    // };
   }
 
   private void debug(Object... args) {
