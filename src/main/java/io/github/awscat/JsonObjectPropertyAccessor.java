12379acc75c21b4dfa76f62b22128f0d979c110f
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-27 06:09:10 -0700

    JsonObjectPropertyAccessor

---
+++ src/main/java/io/github/awscat/JsonObjectPropertyAccessor.java
@@ -0,0 +1,53 @@
+package io.github.awscat;
+
+import java.math.BigDecimal;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import org.springframework.expression.AccessException;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.PropertyAccessor;
+import org.springframework.expression.TypedValue;
+import org.springframework.lang.Nullable;
+
+public class JsonObjectPropertyAccessor implements PropertyAccessor {
+
+	@Override
+	public Class<?>[] getSpecificTargetClasses() {
+		return new Class<?>[] {JsonObject.class};
+	}
+
+	@Override
+	public boolean canRead(EvaluationContext context, @Nullable Object target, String name) throws AccessException {
+		return true;
+	}
+
+	@Override
+	public TypedValue read(EvaluationContext context, @Nullable Object target, String name) throws AccessException {
+		JsonObject jsonObject = JsonObject.class.cast(target);
+		JsonElement jsonElement = jsonObject.get(name);
+		Object value = new Gson().fromJson(jsonElement, Object.class);
+
+		// workaround for double/LazilyParsedNumber
+		if (jsonElement.isJsonPrimitive()) {
+			if (jsonElement.getAsJsonPrimitive().isNumber())
+				value = new BigDecimal(jsonElement.getAsString());
+		}
+
+		return new TypedValue(value);
+	}
+
+	@Override
+	public boolean canWrite(EvaluationContext context, @Nullable Object target, String name) throws AccessException {
+		return true;
+	}
+
+	@Override
+	public void write(EvaluationContext context, @Nullable Object target, String name, @Nullable Object newValue) throws AccessException {
+		JsonObject jsonObject = JsonObject.class.cast(target);
+		jsonObject.add(name, new Gson().toJsonTree(newValue));
+	}
+
+}
