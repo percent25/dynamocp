a03ec0b5ff508c21132d31014cdd281974ac2b7a
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-27 07:37:25 -0700

    providers: name()

--- src/main/java/io/github/awscat/InputPluginProvider.java
+++ src/main/java/io/github/awscat/InputPluginProvider.java
@@ -1,6 +1,11 @@
 package io.github.awscat;
 
+import org.springframework.util.ClassUtils;
+
 public interface InputPluginProvider {
+  default String name() {
+    return ClassUtils.getShortName(getClass());
+  }
   boolean canActivate();
   InputPlugin activate() throws Exception;
 }
--- src/main/java/io/github/awscat/OutputPluginProvider.java
+++ src/main/java/io/github/awscat/OutputPluginProvider.java
@@ -2,9 +2,12 @@ package io.github.awscat;
 
 import java.util.function.Supplier;
 
-import org.springframework.boot.ApplicationArguments;
+import org.springframework.util.ClassUtils;
 
 public interface OutputPluginProvider {
+  default String name() {
+    return ClassUtils.getShortName(getClass());
+  }
   default int mtu() {
     return -1;
   }
