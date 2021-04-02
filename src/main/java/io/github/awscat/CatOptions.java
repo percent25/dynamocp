d747c0e1b2ec05d4f48034f6abac05a5530db669
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-04-02 06:24:37 -0700

    fix: help

--- src/main/java/io/github/awscat/CatOptions.java
+++ src/main/java/io/github/awscat/CatOptions.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.google.gson.Gson;
 
 public class CatOptions {
+  public boolean help;
   public String filter = "true";
   public final List<String> modify = new ArrayList<>();
   public String toString() {
