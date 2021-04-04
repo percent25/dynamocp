ea5b7a93817e929d4dd16b114ef3ccd858d64cc4
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-04-04 15:20:11 -0700

    stress test

--- src/test/java/helpers/FutureRunnerTest.java
+++ src/test/java/helpers/FutureRunnerTest.java
@@ -2,6 +2,7 @@ package helpers;
 
 import java.util.Random;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
 
 import com.google.common.util.concurrent.*;
 
@@ -136,4 +137,49 @@ public class FutureRunnerTest {
     System.out.println("done");
   }
 
+  private final AtomicInteger count = new AtomicInteger();
+
+  @Test
+  public void stressTest() throws Exception {
+    final int desiredCount = 10000;
+    final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    try {
+      ListenableFuture<?> lf = new FutureRunner() {
+        {
+          for (int i = 0; i < desiredCount; ++i) {
+            if (new Random().nextInt() < Integer.MAX_VALUE / 2) {
+              run(() -> {
+                return Futures.immediateVoidFuture();
+              }, ()->{
+                count.incrementAndGet();
+              });
+            } else {
+              run(() -> {
+                return Futures.submit(() -> {
+                  Thread.sleep(2);
+                  return Futures.immediateVoidFuture();
+                }, executor);
+              }, ()->{
+                count.incrementAndGet();
+              });
+            }
+  
+          }
+        }
+        @Override
+        protected void onLanded() {
+          ++landedCount;
+        }
+      }.get();
+      lf.addListener(() -> {
+        System.out.println("listener: landedCount=" + landedCount);
+      }, MoreExecutors.directExecutor());
+      lf.get();
+      // Thread.sleep(3000);
+      assertThat(count.get()).isEqualTo(desiredCount);
+    } finally {
+      executor.shutdown();
+    }
+    System.out.println("done");
+  }
 }
