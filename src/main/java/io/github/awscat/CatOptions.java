0343bf2701df12434ad69fe0a40995e5764b3b30
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-05-15 08:24:20 -0700

    document

--- src/main/java/io/github/awscat/CatOptions.java
+++ src/main/java/io/github/awscat/CatOptions.java
@@ -8,7 +8,10 @@ import com.google.gson.Gson;
 public class CatOptions {
   public boolean help;
   public boolean version;
+  
+  // the javascript actions to evaluate
   public final List<String> js = new ArrayList<>();
+  
   public String toString() {
     return new Gson().toJson(this);
   }
