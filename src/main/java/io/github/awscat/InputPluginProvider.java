fdc9c83567683046b6a32e259ef26fa694770923
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-05-16 05:02:02 -0700

    document

--- src/main/java/io/github/awscat/InputPluginProvider.java
+++ src/main/java/io/github/awscat/InputPluginProvider.java
@@ -2,12 +2,42 @@ package io.github.awscat;
 
 import org.springframework.util.ClassUtils;
 
+/**
+ * InputPluginProvider
+ */
 public interface InputPluginProvider {
+
+  /**
+   * name
+   * 
+   * @return
+   */
   default String name() {
     return ClassUtils.getShortName(getClass());
   }
+
+  /**
+   * help
+   * 
+   * @return
+   */
   String help();
+
+  /**
+   * canActivate
+   * 
+   * @param arg e.g., "dynamo:MyTable,limit=1000"
+   * @return
+   */
   boolean canActivate(String arg);
+
+  /**
+   * activate
+   * 
+   * @param arg e.g., "dynamo:MyTable,limit=1000"
+   * @return
+   * @throws Exception
+   */
   InputPlugin activate(String arg) throws Exception;
-  // void deactivate();
+
 }
