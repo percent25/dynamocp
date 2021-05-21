60240637a65e85f1cbe3cc128e2da4746ec03c0e
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-05-21 04:21:43 -0700

    document

--- src/main/java/awscat/plugins/BaseOptions.java
+++ src/main/java/awscat/plugins/BaseOptions.java
@@ -1,6 +1,19 @@
 package awscat.plugins;
 
+/**
+ * base options common for aws plugins
+ */
 public class BaseOptions {
+
+  /**
+   * @see aws-cli --endpoint-url
+   * @see aws-sdk-2-java endpointOverride
+   */
   public String endpoint;
+
+  /**
+   * @see AWS_PROFILE
+   * @see aws-cli --profile
+   */
   public String profile;
 }
