9d83bcfa4cd31e33245b07ee9519dd47bd5035b7
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-06-17 05:18:02 -0700

    checkpoint: aws queue writer

---
+++ src/main/java/awscat/plugins/AwsQueueOutputPluginProvider.java
@@ -0,0 +1,73 @@
+package awscat.plugins;
+
+import java.util.function.Supplier;
+
+import com.google.common.base.MoreObjects;
+import com.google.gson.Gson;
+
+import org.springframework.stereotype.Service;
+
+import awscat.Args;
+import awscat.OutputPlugin;
+import awscat.OutputPluginProvider;
+import helpers.ConcatenatedJsonWriter;
+import helpers.ConcatenatedJsonWriterTransportAwsQueue;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+
+@Service
+public class AwsQueueOutputPluginProvider implements OutputPluginProvider{
+
+    class Options extends AwsOptions {
+        public String toString() {
+            return new Gson().toJson(this);
+        }
+    }
+
+    private String queueArnOrUrl;
+    private Options options;
+
+    @Override
+    public String help() {
+        return "<queue-arn|queue-url>";
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("queueArnOrUrl", queueArnOrUrl).toString();
+    }
+
+    @Override
+    public boolean canActivate(String arg) {
+        queueArnOrUrl = Args.base(arg);
+        options = Args.options(arg, Options.class);
+        if (queueArnOrUrl.matches("arn:(.+):sqs:(.+):(\\d{12}):(.+)"))
+            return true;
+        if (queueArnOrUrl.matches("https://queue.amazonaws.(.*)/(\\d{12})/(.+)"))
+            return true;
+        if (queueArnOrUrl.matches("https://sqs.(.+).amazonaws.(.*)/(\\d{12})/(.+)"))
+            return true;
+        return false;
+    }
+
+    @Override
+    public Supplier<OutputPlugin> activate(String arg) throws Exception {
+        SqsAsyncClient sqsClient = AwsHelper.configClient(SqsAsyncClient.builder(), options).build();
+
+        String queueUrl = queueArnOrUrl;
+        // is it a queue arn? e.g., arn:aws:sqs:us-east-1:000000000000:MyQueue
+        if (queueArnOrUrl.matches("arn:(.+):sqs:(.+):(\\d{12}):(.+)")) {
+            // yes
+            String queueName = queueArnOrUrl.split(":")[5];
+            queueUrl = sqsClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build()).get().queueUrl();
+        }
+
+        // sqs transport is thread-safe
+        ConcatenatedJsonWriter.Transport transport = new ConcatenatedJsonWriterTransportAwsQueue(sqsClient, queueUrl);
+        
+        // ConcatenatedJsonWriter is not thread-safe
+        // which makes ConcatenatedJsonWriterOutputPlugin not thread-safe
+        return ()->new ConcatenatedJsonWriterOutputPlugin(new ConcatenatedJsonWriter(transport));
+    }
+    
+}
---
+++ src/main/java/helpers/ConcatenatedJsonWriterTransportAwsQueue.java
@@ -0,0 +1,56 @@
+package helpers;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.spotify.futures.CompletableFuturesExtra;
+
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+/**
+ * ConcatenatedJsonWriterTransportAwsQueue
+ */
+public class ConcatenatedJsonWriterTransportAwsQueue implements ConcatenatedJsonWriter.Transport {
+    private final SqsAsyncClient client;
+    private final String queueUrl;
+
+    /**
+     * ctor
+     * 
+     * @param client
+     * @param queueUrl
+     */
+    public ConcatenatedJsonWriterTransportAwsQueue(SqsAsyncClient client, String queueUrl) {
+        debug("ctor", queueUrl);
+        this.client = client;
+        this.queueUrl = queueUrl;
+    }
+
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("queueUrl", queueUrl).toString();
+    }
+
+    @Override
+    public int mtu() {
+        return 256 * 1024; // sns/sqs max msg len
+    }
+
+    @Override
+    public ListenableFuture<?> send(String string) {
+        trace("send", string.length());
+        SendMessageRequest request = SendMessageRequest.builder() //
+                .queueUrl(queueUrl) //
+                .messageBody(string) //
+                .build();
+        return CompletableFuturesExtra.toListenableFuture(client.sendMessage(request));
+    }
+
+    private void debug(Object... args) {
+        new LogHelper(this).debug(args);
+    }
+
+    private void trace(Object... args) {
+        new LogHelper(this).trace(args);
+    }
+
+}
