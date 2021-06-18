a81692838909a0f36b4a64911945543483d685b9
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-06-18 04:00:19 -0700

    document

--- src/main/java/awscat/plugins/AwsQueueInputPluginProvider.java
+++ src/main/java/awscat/plugins/AwsQueueInputPluginProvider.java
@@ -18,107 +18,108 @@ import software.amazon.awssdk.services.sqs.*;
 
 class AwsQueueInputPlugin implements InputPlugin {
 
-    private final AwsQueueReceiver receiver;
-    private final int limit;
+  private final AwsQueueReceiver receiver;
+  private final int limit;
 
-    private Function<Iterable<JsonElement>, ListenableFuture<?>> listener;
+  private Function<Iterable<JsonElement>, ListenableFuture<?>> listener;
 
-    private final VoidFuture lf = new VoidFuture();
-    private final AtomicInteger count = new AtomicInteger();
+  private final VoidFuture lf = new VoidFuture();
+  private final AtomicInteger count = new AtomicInteger();
 
-    public AwsQueueInputPlugin(AwsQueueReceiver receiver, int limit) {
-        debug("ctor");
-        this.receiver = receiver;
-        this.limit = limit;
-        receiver.setListener(message -> {
-            return new FutureRunner() {
-                List<JsonElement> list;
-                {
-                    run(() -> {
-                        count.updateAndGet(count -> {
-                            list = Lists.newArrayList(Iterators.limit(new JsonStreamParser(message), limit - count));
-                            return count + list.size();
-                        });
+  public AwsQueueInputPlugin(AwsQueueReceiver receiver, int limit) {
+    debug("ctor");
+    this.receiver = receiver;
+    this.limit = limit;
+    receiver.setListener(message -> {
+      return new FutureRunner() {
+        List<JsonElement> list;
+        {
+          run(() -> {
+            count.updateAndGet(count -> {
+              list = Lists.newArrayList(Iterators.limit(new JsonStreamParser(message), limit - count));
+              return count + list.size();
+            });
 
-                        return listener.apply(list);
-                    }, () -> {
-                        if (count.get() == limit) {
-                            try {
-                                receiver.close();
-                            } finally {
-                                lf.setVoid();
-                            }
-                        }
-                    });
-                }
-            };
-        });
-    }
+            return listener.apply(list);
+          }, () -> {
+            if (count.get() == limit) {
+              try {
+                receiver.close();
+              } finally {
+                lf.setVoid();
+              }
+            }
+          });
+        }
+      };
+    });
+  }
 
-    public String toString() {
-        return MoreObjects.toStringHelper(this).add("receiver", receiver).toString();
-    }
+  public String toString() {
+    return MoreObjects.toStringHelper(this).add("receiver", receiver).toString();
+  }
 
-    @Override
-    public ListenableFuture<?> read(int mtu) throws Exception {
-        receiver.start();
-        return lf;
-    }
+  @Override
+  public ListenableFuture<?> read(int mtu) throws Exception {
+    receiver.start();
+    return lf;
+  }
 
-    @Override
-    public void setListener(Function<Iterable<JsonElement>, ListenableFuture<?>> listener) {
-        this.listener = listener;
-    }
+  @Override
+  public void setListener(Function<Iterable<JsonElement>, ListenableFuture<?>> listener) {
+    this.listener = listener;
+  }
 
-    private void debug(Object... args) {
-        new LogHelper(this).debug(args);
-    }
+  private void debug(Object... args) {
+    new LogHelper(this).debug(args);
+  }
 
 }
 
 class AwsQueueInputPluginOptions extends AwsOptions {
-    public int c;
-    public int limit;
+  public int c;
+  public int limit;
 
-    public String toString() {
-        return new Gson().toJson(this);
-    }
+  public String toString() {
+    return new Gson().toJson(this);
+  }
 }
 
 @Service
 public class AwsQueueInputPluginProvider extends AbstractInputPluginProvider {
 
-    private String queueArnOrUrl;
-    private AwsQueueInputPluginOptions options;
+  private String queueArnOrUrl;
+  private AwsQueueInputPluginOptions options;
 
-    public AwsQueueInputPluginProvider() {
-        super("<queue-arn|queue-url>", AwsQueueInputPluginOptions.class);
-    }
+  public AwsQueueInputPluginProvider() {
+    super("<queue-arn|queue-url>", AwsQueueInputPluginOptions.class);
+  }
 
-    @Override
-    public String toString() {
-        // return new Gson().toJson(this);
-        return MoreObjects.toStringHelper(this).add("queueArnOrUrl", queueArnOrUrl).add("options", options).toString();
-      }
+  @Override
+  public String toString() {
+    // return new Gson().toJson(this);
+    return MoreObjects.toStringHelper(this).add("queueArnOrUrl", queueArnOrUrl).add("options", options).toString();
+  }
 
-    @Override
-    public boolean canActivate(String arg) {
-        queueArnOrUrl = Args.base(arg);
-        options = Args.options(arg, AwsQueueInputPluginOptions.class);
-        if (queueArnOrUrl.matches("arn:(.+):sqs:(.+):(\\d{12}):(.+)"))
-            return true;
-        if (queueArnOrUrl.matches("https://queue.amazonaws.(.*)/(\\d{12})/(.+)"))
-            return true;
-        if (queueArnOrUrl.matches("https://sqs.(.+).amazonaws.(.*)/(\\d{12})/(.+)"))
-            return true;
-        return false;
-    }
+  // https://docs.aws.amazon.com/general/latest/gr/sqs-service.html
+  @Override
+  public boolean canActivate(String arg) {
+    queueArnOrUrl = Args.base(arg);
+    options = Args.options(arg, AwsQueueInputPluginOptions.class);
+    if (queueArnOrUrl.matches("arn:(.+):sqs:(.+):(\\d{12}):(.+)"))
+      return true;
+    if (queueArnOrUrl.matches("https://queue.amazonaws.(.*)/(\\d{12})/(.+)"))
+      return true;
+    if (queueArnOrUrl.matches("https://sqs.(.+).amazonaws.(.*)/(\\d{12})/(.+)"))
+      return true;
+    return false;
+  }
 
-    @Override
-    public InputPlugin activate(String arg) throws Exception {
-        int c = options.c > 0 ? options.c : Runtime.getRuntime().availableProcessors();
-        SqsAsyncClient sqsClient = AwsHelper.configClient(SqsAsyncClient.builder(), options).build();
-        return new AwsQueueInputPlugin(new AwsQueueReceiver(sqsClient, queueArnOrUrl, c), options.limit);
-    }
+  @Override
+  public InputPlugin activate(String arg) throws Exception {
+    int c = options.c > 0 ? options.c : Runtime.getRuntime().availableProcessors();
+    SqsAsyncClient sqsClient = AwsHelper.configClient(SqsAsyncClient.builder(), options).build();
+    return new AwsQueueInputPlugin(new AwsQueueReceiver(sqsClient, queueArnOrUrl, c), options.limit);
+  }
 
 }
