3c8137c41f63a33467a2b030dab936a6798757bb
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-06-18 08:11:05 -0700

    don't use logHelper.str

--- src/main/java/helpers/LogHelper.java
+++ src/main/java/helpers/LogHelper.java
@@ -1,7 +1,5 @@
 package helpers;
 
-import java.util.*;
-
 import com.google.common.base.*;
 import com.google.common.collect.*;
 
@@ -22,25 +20,8 @@ public class LogHelper {
     this.self = self;
   }
 
-  public String str(Object... args) {
-    List<String> parts = Lists.newArrayList();
-    // parts.add(new Date().toString());
-    // parts.add(String.format("[%s]", Thread.currentThread().getName()));
-    // parts.add(ClassUtils.getShortName(self.getClass()));
-    for (Object arg : args)
-      parts.add("" + arg);
-    return String.join(" ", parts);
-  }
-  
-  // public void stdout(Object... args) {
-  //   System.out.println(str(args));
-  // }
-
-  // public void stderr(Object... args) {
-  //   System.err.println(str(args));
-  // }
-
   public void debug(Object... args) {
+    // System.out.println(Lists.newArrayList(args));
     LoggerFactory.getLogger(self.getClass()).debug(Strings.repeat("{} ", args.length), args);
   }
 
