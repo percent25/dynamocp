46ee69c32dfd6fe40ac17c2efc67fff018a8e1e4
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-06-22 02:55:47 -0700

    tidyup

--- src/main/java/awscat/Addresses.java
+++ src/main/java/awscat/Addresses.java
@@ -6,8 +6,6 @@ import java.util.*;
 import com.google.common.base.*;
 import com.google.gson.*;
 
-import org.springframework.boot.*;
-
 // arn:aws:dynamo:us-east-1:102938475610:table/MyTable,c=1,delete=true,wcu=5
 public class Addresses { //###TODO RENAME TO Addresses
     /**
@@ -45,35 +43,5 @@ public class Addresses { //###TODO RENAME TO Addresses
         }
         return new Gson().fromJson(new Gson().toJson(options), typeOfT);
     }
-    // public static <T> T options(String arg, Class<T> classOfT) { //###TODO RENAME TO ADDRESSOPTIONS
-    //     return options(arg, (Type) classOfT);
-    // }
-  
-    // public static String source(ApplicationArguments args) {
-    //     return parseArg(args.getNonOptionArgs().get(0));
-    // }
 
-    // public static String target(ApplicationArguments args) {
-    //     return parseArg(args.getNonOptionArgs().get(1));
-    // }
-
-    // public static <T> T parseOptions(ApplicationArguments args, Class<T> classOfT) throws Exception {
-    //     JsonObject options = new Gson().toJsonTree(classOfT.getConstructor().newInstance()).getAsJsonObject();
-    //     for (String name : args.getOptionNames()) {
-    //         String lowerCamel = CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, name);
-    //         if (args.getOptionValues(name).size()==0) {
-    //             options.addProperty(lowerCamel, true);
-    //         } else {
-    //             for (String value : args.getOptionValues(name)) {
-    //                 JsonElement jsonElement = options.get(lowerCamel);
-    //                 if (jsonElement == null || !jsonElement.isJsonArray())
-    //                     options.addProperty(lowerCamel, value);
-    //                 else
-    //                     jsonElement.getAsJsonArray().add(value);
-    //             }    
-    //         }
-    //     }
-    //     return new Gson().fromJson(options, classOfT);
-    // }
-    
 }
