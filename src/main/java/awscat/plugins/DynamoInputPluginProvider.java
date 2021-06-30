2382500a04b56eb94398216ab97aaa9344655cff
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-06-30 05:43:12 -0700

    use DynamoReader

--- src/main/java/awscat/plugins/DynamoInputPluginProvider.java
+++ src/main/java/awscat/plugins/DynamoInputPluginProvider.java
@@ -18,156 +18,35 @@ import helpers.*;
 import software.amazon.awssdk.services.dynamodb.*;
 import software.amazon.awssdk.services.dynamodb.model.*;
 
-// https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.ReadWriteCapacityMode.html
 class DynamoInputPlugin implements InputPlugin {
 
-  private final DynamoDbAsyncClient client;
-  private final String tableName;
-  private final Iterable<String> keySchema;
-  private final int totalSegments;
-  private final AbstractThrottle readLimiter;
   private final int limit;
-
-  //### TODO THIS DOES NOT NEED TO BE A MEMBER VARIABLE
-  //### TODO THIS DOES NOT NEED TO BE A MEMBER VARIABLE
-  //### TODO THIS DOES NOT NEED TO BE A MEMBER VARIABLE
-  private final List<Map<String, AttributeValue>> exclusiveStartKeys = Lists.newArrayList();
-  //### TODO THIS DOES NOT NEED TO BE A MEMBER VARIABLE
-  //### TODO THIS DOES NOT NEED TO BE A MEMBER VARIABLE
-  //### TODO THIS DOES NOT NEED TO BE A MEMBER VARIABLE
-
-  private Function<Iterable<JsonElement>, ListenableFuture<?>> listener;
+  private final DynamoReader dynamoReader;
 
   /**
    * ctor
    * 
    * @param client
    * @param tableName
-   * @param keySchema
    * @param totalSegments
    * @param readLimiter
    * @param limit
    */
-  public DynamoInputPlugin(DynamoDbAsyncClient client, String tableName, Iterable<String> keySchema, int totalSegments, AbstractThrottle readLimiter, int limit) {
+  public DynamoInputPlugin(DynamoDbAsyncClient client, String tableName, int totalSegments, AbstractThrottle readLimiter, int limit) {
     debug("ctor");
-
-    this.client = client;
-    this.tableName = tableName;
-    this.keySchema = keySchema;
-    this.totalSegments = totalSegments;
-    this.readLimiter = readLimiter;
     this.limit = limit;
-
-    // https://aws.amazon.com/blogs/developer/rate-limited-scans-in-amazon-dynamodb
-    exclusiveStartKeys.addAll(Collections.nCopies(totalSegments, null));
-  }
-
-  public String toString() {
-    return MoreObjects.toStringHelper(this)
-        //
-        .add("tableName", tableName)
-        //
-        .add("keySchema", keySchema)
-        //
-        .add("totalSegments", totalSegments)
-        //
-        .add("readLimiter", readLimiter)
-        //
-        // .add("limit", limit)
-        //
-        .toString();
+    dynamoReader = new DynamoReader(client, tableName, totalSegments, readLimiter);
   }
 
   @Override
   public void setListener(Function<Iterable<JsonElement>, ListenableFuture<?>> listener) {
-    this.listener = listener;
+    dynamoReader.setListener(listener);
   }
 
   @Override
   public ListenableFuture<?> run(int mtu) throws Exception {
-    debug("read", "mtu", mtu);
-
-    return new FutureRunner() {
-      {
-        for (int segment = 0; segment < totalSegments; ++segment)
-          doSegment(segment);
-      }
-      void doSegment(int segment) {
-        
-        debug("doSegment", segment);
-
-        run(() -> {
-  
-          // pre-throttle
-          // https://aws.amazon.com/blogs/developer/rate-limited-scans-in-amazon-dynamodb
-          //###TODO make async
-          //###TODO make async
-          //###TODO make async
-          readLimiter.asyncAcquire(128).get(); //###TODO
-          //###TODO make async
-          //###TODO make async
-          //###TODO make async
-
-          // STEP 1 Do the scan
-          ScanRequest scanRequest = ScanRequest.builder()
-              //
-              .tableName(tableName)
-              //
-              .exclusiveStartKey(exclusiveStartKeys.get(segment))
-              //
-              // .returnConsumedCapacity(ReturnConsumedCapacity.TOTAL)
-              //
-              .segment(segment)
-              //
-              .totalSegments(totalSegments)
-              //
-              // .limit(256)
-              //
-              .build();
-
-          if (limit > 0)
-            scanRequest = scanRequest.toBuilder().limit(limit).build();
-
-          debug("doSegment", segment, scanRequest);
-
-          return lf(client.scan(scanRequest));
-        }, scanResponse -> {
-
-          debug("doSegment", segment, scanResponse.count());
-
-          exclusiveStartKeys.set(segment, scanResponse.lastEvaluatedKey());
-
-          // STEP 2 Process the scan
-          List<ListenableFuture<?>> futures = new ArrayList<>();
-
-          List<JsonElement> jsonElements = new ArrayList<>();
-          for (Map<String, AttributeValue> item : scanResponse.items())
-            jsonElements.add(DynamoHelper.parse(item));
-          if (jsonElements.size()>0) {
-            for (List<JsonElement> partition : Lists.partition(jsonElements, mtu>0?mtu:jsonElements.size())) {
-              run(()->{
-                ListenableFuture<?> lf = listener.apply(partition);
-                futures.add(lf);
-                return lf;
-              });
-            }  
-          }
-
-          run(()->{
-            return Futures.allAsList(futures);
-          }, result->{ // if success then continue
-            if (!exclusiveStartKeys.get(segment).isEmpty()) //###TODO check for null here?
-              doSegment(segment);
-          });
-
-        }, e->{
-          debug("doSegment", segment, e);
-          e.printStackTrace();
-          throw new RuntimeException(e);
-        });
-
-      }
-    };
+    debug("run", "mtu", mtu);
+    return dynamoReader.scan(mtu);
   }
 
   private void debug(Object... args) {
@@ -221,7 +100,7 @@ public class DynamoInputPluginProvider extends AbstractInputPluginProvider {
       }
     }, 25, TimeUnit.SECONDS);
     
-    Iterable<String> keySchema = Lists.transform(describeTable.get().table().keySchema(), e->e.attributeName());      
+    // Iterable<String> keySchema = Lists.transform(describeTable.get().table().keySchema(), e->e.attributeName());      
 
     // https://aws.amazon.com/blogs/developer/rate-limited-scans-in-amazon-dynamodb/
     int c = Runtime.getRuntime().availableProcessors();
@@ -241,7 +120,7 @@ public class DynamoInputPluginProvider extends AbstractInputPluginProvider {
       return Double.MAX_VALUE; // on-demand/pay-per-request
     });
 
-    return new DynamoInputPlugin(asyncClient, tableName, keySchema, c, readLimiter, options.limit);
+    return new DynamoInputPlugin(asyncClient, tableName, c, readLimiter, options.limit);
   }
   
   private void debug(Object... args) {
