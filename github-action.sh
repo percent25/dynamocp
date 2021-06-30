b730adfb902ac638fdad60bdd16615f5050c8237
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-06-30 07:39:09 -0700

    github-action.sh

--- github-action.sh
+++ github-action.sh
@@ -1,30 +1,41 @@
 #!/bin/sh -ex
 
-echo ${GITHUB_RUN_ID?}
-echo ${GITHUB_SHA?}
+#echo ${GITHUB_RUN_ID?}
+#echo ${GITHUB_SHA?}
 
-REVISION=v0.9.${GITHUB_RUN_ID}-${GITHUB_SHA}
+REVISION=$(date +%Y.%m).${GITHUB_RUN_ID?}
 
-./build.sh ${REVISION?}
+# ./build.sh ${REVISION?}
+
+./mvnw package -B -Drevision=${REVISION?} -Plocalstack
+
+ln -fs target/awscat-${REVISION?}.jar awscat.jar
+
+cat > setup.py << EOF
+import setuptools
+setuptools.setup(name="awscat", version="${REVISION?}", scripts=["awscat.jar"])
+EOF
+
+zip awscat.zip awscat.jar setup.py
 
 # TODO
 # TODO
 # TODO
-exit 0
+# exit 0
 # TODO
 # TODO
 # TODO
 
 response_code=$(curl -sv -o output -w "%{response_code}" -H"Authorization: Token ${GITHUB_TOKEN?}" https://api.github.com/repos/percent25/awscat/releases -d @-) << EOF
 {
-  "tag_name": "${REVISION}",
-  "target_commitish": "${GITHUB_SHA}"
+  "tag_name": "${REVISION?}",
+  "target_commitish": "${GITHUB_SHA?}"
 }
 EOF
 cat output
 
 if [ $response_code -lt 400 ]; then
   release_id=$(jq .id output)
-  response_code=$(curl -sv -o output -w "%{response_code}" -H"Authorization: token ${GITHUB_TOKEN?}" -H"Content-Type: application/octet-stream" "https://uploads.github.com/repos/percent25/awscat/releases/$release_id/assets?name=awscat.zip" --data-binary @awscat.zip)
+  response_code=$(curl -sv -o output -w "%{response_code}" -H"Authorization: Token ${GITHUB_TOKEN?}" -H"Content-Type: application/octet-stream" "https://uploads.github.com/repos/percent25/awscat/releases/${release_id?}/assets?name=awscat.zip" --data-binary @awscat.zip)
   cat output
 fi
