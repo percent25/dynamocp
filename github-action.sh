88651d91b88331b36302ae85076b23213dfd0c11
rrizun <rrizun@rrizuns-MacBook-Air.local>
2022-02-18 04:49:21 -0800

    document

--- github-action.sh
+++ github-action.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
 # https://maven.apache.org/maven-ci-friendly.html
-REVISION=$(date +%Y.%m).$(echo ${GITHUB_SHA?} | head -c 7)
+REVISION=$(date +%Y.%m).$(echo ${GITHUB_SHA?} | head -c 7) # e.g., 2021.09.da16b76
 
 ./mvnw verify -B -Drevision=${REVISION?} -Plocalstack
 
@@ -14,6 +14,7 @@ EOF
 
 zip awscat.zip awscat.jar setup.py
 
+# create github release
 response_code=$(curl -sv -o output -w "%{response_code}" -H"Authorization: Token ${GITHUB_TOKEN?}" https://api.github.com/repos/percent25/awscat/releases -d @-) << EOF
 {
   "tag_name": "${REVISION?}",
@@ -22,6 +23,7 @@ response_code=$(curl -sv -o output -w "%{response_code}" -H"Authorization: Token
 EOF
 cat output
 
+# upload github release binary
 if [ $response_code -lt 400 ]; then
   release_id=$(jq .id output)
   response_code=$(curl -sv -o output -w "%{response_code}" -H"Authorization: Token ${GITHUB_TOKEN?}" -H"Content-Type: application/octet-stream" "https://uploads.github.com/repos/percent25/awscat/releases/${release_id?}/assets?name=awscat.zip" --data-binary @awscat.zip)
