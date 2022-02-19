4c8b6b28752b97a17ed2ee41d05f633a9fa55a71
rrizun <rrizun@rrizuns-MacBook-Air.local>
2022-02-19 08:26:17 -0800

    check error

--- github-action.sh
+++ github-action.sh
@@ -28,4 +28,9 @@ if [ $response_code -lt 400 ]; then
   release_id=$(jq .id output)
   response_code=$(curl -sv -o output -w "%{response_code}" -H"Authorization: Token ${GITHUB_TOKEN?}" -H"Content-Type: application/octet-stream" "https://uploads.github.com/repos/percent25/awscat/releases/${release_id?}/assets?name=awscat.zip" --data-binary @awscat.zip)
   cat output
+  if [ $response_code -lt 400 ]; then
+    exit 0
+  fi
 fi
+
+exit 1
